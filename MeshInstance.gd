d189d7d803cdf6cd9c341c23c6934165e852dc70
Connor Hughes <connorhughes341@gmail.com>
2020-02-01 11:21:10 -0500

    improved animation

---
+++ MeshInstance.gd
@@ -0,0 +1,18 @@
+extends MeshInstance
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var material = SpatialMaterial.new()
+	material = get_surface_material(0)
+	#material.albedo_color = Color(1,0,0)
+	#material.set_surface_material(0,material)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
