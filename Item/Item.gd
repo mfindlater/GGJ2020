0cb35fca2bc004e3152fca091fb00bdc0d76f9db
Matthew Findlater <matthew.findlater@gmail.com>
2020-02-01 18:53:44 -0500

    fix signals

--- Item/Item.gd
+++ Item/Item.gd
@@ -4,6 +4,10 @@ signal item_enter
 signal item_exit
 
 func _ready():
+	var player = get_parent().get_node("Player")
+	player.connect("item_enter", player, "_on_Item_item_enter")
+	player.connect("item_exit", player, "_on_Item_item_exit")
+	
 	add_to_group("items")
 
 func _on_Area_body_entered(body):
