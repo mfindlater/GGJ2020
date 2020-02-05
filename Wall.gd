27e2f5ba42e14359458a1cde44ce1fe648ad7d89
Matthew Findlater <matthew.findlater@gmail.com>
2020-02-05 00:36:00 -0500

    audio and hide fixes

--- Wall.gd
+++ Wall.gd
@@ -6,7 +6,15 @@ func _ready():
 func _on_Wall_body_entered(body):
 	if body.has_meta("player"):
 		get_parent().hide()
+		
+		var hiders = get_tree().get_nodes_in_group("hide")
+		for hider in hiders:
+			hider.hide()
 
 func _on_Wall_body_exited(body):
 	if body.has_meta("player"):
 		get_parent().show()
+		
+		var hiders = get_tree().get_nodes_in_group("hide")
+		for hider in hiders:
+			hider.hide()
